b6aa4da732c17f61b965cf248470ab3d6a465246
mtjohnson47 <mtjohnson47@gmail.com>
2014-05-02 16:49:53 -0500

    Program skeleton

---
+++ electrodia/src/org/electrodia/ElectrodiaMain.java
@@ -0,0 +1,11 @@
+package org.electrodia;
+
+import org.electrodia.display.ElectrodiaWindow;
+
+public class ElectrodiaMain {
+
+	public static void main(String[] args) {
+		new ElectrodiaWindow();
+	}
+
+}
---
+++ electrodia/src/org/electrodia/display/ElectrodiaWindow.java
@@ -0,0 +1,12 @@
+package org.electrodia.display;
+
+import javax.swing.JFrame;
+
+public class ElectrodiaWindow extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	public ElectrodiaWindow(){
+		
+	}
+	
+}

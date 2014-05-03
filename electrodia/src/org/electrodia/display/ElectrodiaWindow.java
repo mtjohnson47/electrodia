0ec99bbd7cf8da2664f4e3748b72aa7174c8a794
mtjohnson47 <mtjohnson47@gmail.com>
2014-05-02 21:17:39 -0500

    Added Event Handlers for menu items

--- electrodia/src/org/electrodia/display/ElectrodiaWindow.java
+++ electrodia/src/org/electrodia/display/ElectrodiaWindow.java
@@ -1,20 +1,23 @@
 package org.electrodia.display;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JFrame;
-import javax.swing.JMenuBar;
 import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JSeparator;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
 import javax.swing.KeyStroke;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-
-import java.awt.event.KeyEvent;
-import java.awt.event.InputEvent;
-import javax.swing.JSplitPane;
-import java.awt.BorderLayout;
-import javax.swing.JTabbedPane;
-import javax.swing.JPanel;
 
 public class ElectrodiaWindow extends JFrame {
 	private static final long serialVersionUID = 2L;
@@ -59,6 +62,21 @@ public class ElectrodiaWindow extends JFrame {
 	private JPanel projectFileHolder;
 	private JTabbedPane tabbedPane;
 	
+	private final Action newButtonClicked = new NewButtonClicked();
+	private final Action openButtonClicked = new OpenButtonClicked();
+	private final Action saveButtonClicked = new SaveButtonClicked();
+	private final Action saveAsButtonClicked = new SaveAsButtonClicked();
+	private final Action closeCurrentButtonClicked = new CloseCurrentButtonClicked();
+	private final Action closeAllButtonClicked = new CloseAllButtonClicked();
+	private final Action closeAllButCurrentButtonClicked = new CloseAllButCurrentButtonClicked();
+	private final Action exitApplicationButtonClicked = new ExitApplicationButtonClicked();
+	private final Action copyButtonClicked = new CopyButtonClicked();
+	private final Action cutButtonClicked = new CutButtonClicked();
+	private final Action pasteButtonClicked = new PasteButtonClicked();
+	private final Action preferencesButtonClicked = new PreferencesButtonClicked();
+	private final Action helpButtonClicked = new HelpButtonClicked();
+	private final Action aboutButtonClicked = new AboutButtonClicked();
+	
 	public ElectrodiaWindow(){
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		try {
@@ -75,11 +93,13 @@ public class ElectrodiaWindow extends JFrame {
 		mnFile = new JMenu("File");
 		menuBar.add(mnFile);
 		
-		mntmNew = new JMenuItem("New...");
+		mntmNew = new JMenuItem();
+		mntmNew.setAction(newButtonClicked);
 		mntmNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_MASK));
 		mnFile.add(mntmNew);
 		
-		mntmOpen = new JMenuItem("Open...");
+		mntmOpen = new JMenuItem();
+		mntmOpen.setAction(openButtonClicked);
 		mntmOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
 		mnFile.add(mntmOpen);
 		
@@ -87,10 +107,12 @@ public class ElectrodiaWindow extends JFrame {
 		mnFile.add(separator);
 		
 		mntmSave = new JMenuItem("Save");
+		mntmSave.setAction(saveButtonClicked);
 		mntmSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
 		mnFile.add(mntmSave);
 		
 		mntmSaveAs = new JMenuItem("Save as...");
+		mntmSaveAs.setAction(saveAsButtonClicked);
 		mntmSaveAs.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK | InputEvent.SHIFT_MASK));
 		mnFile.add(mntmSaveAs);
 		
@@ -116,17 +138,21 @@ public class ElectrodiaWindow extends JFrame {
 		mnFile.add(mnClose);
 		
 		mntmCurrent = new JMenuItem("Current");
+		mntmCurrent.setAction(closeCurrentButtonClicked);
 		mntmCurrent.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, InputEvent.CTRL_MASK));
 		mnClose.add(mntmCurrent);
 		
 		mntmAllExceptCurrent = new JMenuItem("All except current");
+		mntmAllExceptCurrent.setAction(closeAllButCurrentButtonClicked);
 		mnClose.add(mntmAllExceptCurrent);
 		
 		mntmAll = new JMenuItem("All");
+		mntmAll.setAction(closeAllButtonClicked);
 		mntmAll.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, InputEvent.CTRL_MASK | InputEvent.ALT_MASK));
 		mnClose.add(mntmAll);
 		
 		mntmExit = new JMenuItem("Exit");
+		mntmExit.setAction(exitApplicationButtonClicked);
 		mntmExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F4, InputEvent.ALT_MASK));
 		mnFile.add(mntmExit);
 		
@@ -134,14 +160,17 @@ public class ElectrodiaWindow extends JFrame {
 		menuBar.add(mnEdit);
 		
 		mntmCopy = new JMenuItem("Copy");
+		mntmCopy.setAction(copyButtonClicked);
 		mntmCopy.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_MASK));
 		mnEdit.add(mntmCopy);
 		
 		mntmCut = new JMenuItem("Cut");
+		mntmCut.setAction(cutButtonClicked);
 		mntmCut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.CTRL_MASK));
 		mnEdit.add(mntmCut);
 		
 		mntmPaste = new JMenuItem("Paste");
+		mntmPaste.setAction(pasteButtonClicked);
 		mntmPaste.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_MASK));
 		mnEdit.add(mntmPaste);
 		
@@ -149,6 +178,7 @@ public class ElectrodiaWindow extends JFrame {
 		menuBar.add(mnOptions);
 		
 		mntmPreferences = new JMenuItem("Preferences...");
+		mntmPreferences.setAction(preferencesButtonClicked);
 		mntmPreferences.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, InputEvent.ALT_MASK));
 		mnOptions.add(mntmPreferences);
 		
@@ -156,6 +186,7 @@ public class ElectrodiaWindow extends JFrame {
 		menuBar.add(mnHelp);
 		
 		mntmContents = new JMenuItem("Contents...");
+		mntmContents.setAction(helpButtonClicked);
 		mntmContents.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0));
 		mnHelp.add(mntmContents);
 		
@@ -163,6 +194,7 @@ public class ElectrodiaWindow extends JFrame {
 		mnHelp.add(separator_3);
 		
 		mntmAbout = new JMenuItem("About...");
+		mntmAbout.setAction(aboutButtonClicked);
 		mntmAbout.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F1, InputEvent.ALT_MASK));
 		mnHelp.add(mntmAbout);
 		
@@ -216,4 +248,143 @@ public class ElectrodiaWindow extends JFrame {
 		
 	}
 	
+	private class NewButtonClicked extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+		public NewButtonClicked() {
+			putValue(NAME, "New...");
+			putValue(SHORT_DESCRIPTION, "Creates a new project");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	
+	private class OpenButtonClicked extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+		public OpenButtonClicked() {
+			putValue(NAME, "Open...");
+			putValue(SHORT_DESCRIPTION, "Opens an existing project");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	
+	private class SaveButtonClicked extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+		public SaveButtonClicked() {
+			putValue(NAME, "Save");
+			putValue(SHORT_DESCRIPTION, "Saves the project to disk.");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	
+	private class SaveAsButtonClicked extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+		public SaveAsButtonClicked() {
+			putValue(NAME, "Save as...");
+			putValue(SHORT_DESCRIPTION, "Creates a duplicate project and saves changes to it.");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	
+	private class CloseCurrentButtonClicked extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+		public CloseCurrentButtonClicked() {
+			putValue(NAME, "Close current");
+			putValue(SHORT_DESCRIPTION, "Closes the current project, but leaves all others open.");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	
+	private class CloseAllButtonClicked extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+		public CloseAllButtonClicked() {
+			putValue(NAME, "Close all");
+			putValue(SHORT_DESCRIPTION, "Closes all projects.");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	
+	private class CloseAllButCurrentButtonClicked extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+		public CloseAllButCurrentButtonClicked() {
+			putValue(NAME, "Close all but current");
+			putValue(SHORT_DESCRIPTION, "Closes all projects, but leaves the current one open.");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	
+	private class ExitApplicationButtonClicked extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+		public ExitApplicationButtonClicked() {
+			putValue(NAME, "Exit Application");
+			putValue(SHORT_DESCRIPTION, "Closes the Electrodia Window");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	
+	private class CopyButtonClicked extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+		public CopyButtonClicked() {
+			putValue(NAME, "Copy");
+			putValue(SHORT_DESCRIPTION, "Copies whatever is currently selected");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	
+	private class CutButtonClicked extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+		public CutButtonClicked() {
+			putValue(NAME, "Cut");
+			putValue(SHORT_DESCRIPTION, "Copies whatever is currently selected, and then removes it");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	
+	private class PasteButtonClicked extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+		public PasteButtonClicked() {
+			putValue(NAME, "Paste");
+			putValue(SHORT_DESCRIPTION, "Pastes the contents of the clipboard into project");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	
+	private class PreferencesButtonClicked extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+		public PreferencesButtonClicked() {
+			putValue(NAME, "Preferences...");
+			putValue(SHORT_DESCRIPTION, "Opens the global preferences dialog");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	
+	private class HelpButtonClicked extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+		public HelpButtonClicked() {
+			putValue(NAME, "Contents");
+			putValue(SHORT_DESCRIPTION, "Opens the help window");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	
+	private class AboutButtonClicked extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+		public AboutButtonClicked() {
+			putValue(NAME, "About");
+			putValue(SHORT_DESCRIPTION, "Opens a window with Electrodia version information");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
 }
